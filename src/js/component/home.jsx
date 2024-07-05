e8ed58f2f227f63658c6eae13dbcd346d1a4b2b2
Letimachado <leti.machado.gerez@gmail.com>
2024-07-05 20:34:39 +0000

    Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Digito } from "./numeros";
 
 //create your first component
 const Home = () => {
+
+	const [timer, setTimer] = useState(0)
+
+	useEffect(()=>{
+		setTimeout(()=>{
+			setTimer(value=> value+1)}, 1000)},[timer])
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="counter-holder">
+			<Digito number={<span className="fa fa-clock"></span>}/>
+			<Digito number={Math.floor(timer/100000)%10}/>
+			<Digito number={Math.floor(timer/10000)%10}/>
+			<Digito number={Math.floor(timer/1000)%10}/>
+			<Digito number={Math.floor(timer/100)%10}/>
+			<Digito number={Math.floor(timer/10)%10}/>
+			<Digito number={Math.floor(timer%10)}/>
+			</div>
 		</div>
 	);
 };
---
+++ src/js/component/numeros.jsx
@@ -0,0 +1,4 @@
+import { number } from "prop-types";
+import React from "react";
+
+export const Digito = ({number}) => <div className="digit">{number}</div>
